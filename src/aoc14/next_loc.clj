13cb0f3a8999b9fb08c1280572a9105a469e3967
Fenton Travers <fenton.travers@gmail.com>
2019-01-05 11:16:39 -0800

    tests passing for next-location

--- src/aoc14/next_loc.clj
+++ src/aoc14/next_loc.clj
@@ -4,28 +4,22 @@
   (def locations [0 1])
   (def recipes [3 7 1 0]))
 
+(defn one-elf-next-loc [length curr-index jump]
+  (mod (+ curr-index jump) length))
+
 (defn next-location [recipes locations]
   (let [a-loc (locations 0)
         b-loc (locations 1)
         a-jump (+ 1 (recipes a-loc))
         b-jump (+ 1 (recipes b-loc))
         a-next-loc (one-elf-next-loc (count recipes) a-loc a-jump)
-        b-next-loc (one-elf-next-loc (count recipes) b-loc b-jump)
-
-        ]
-
-    [a-loc b-loc a-jump a-next-loc b-jump b-next-loc])
-;; => [0 1 4 0 8 4] 
-
-  )
+        b-next-loc (one-elf-next-loc (count recipes) b-loc b-jump)]
+    [a-next-loc b-next-loc]))
 
 (comment
   (def length 4)
   (def curr-index 1)
   (def jump 8))
-
-(defn one-elf-next-loc [length curr-index jump]
-  (mod (+ curr-index jump) length))
 
 (comment
   (next-location recipes locations))
--- test/aoc14/next_loc_test.clj
+++ test/aoc14/next_loc_test.clj
@@ -1,9 +1,36 @@
 (ns aoc14.next-loc-test
   (:require [aoc14.next-loc :as sut]
             [clojure.test :refer :all ]))
-
 (deftest next-location-tests
+  (testing ""
+    (is (let [recipes [3 7 1 0]
+              locations [0 1]
+              next-locations [0 1]]
+          (= next-locations (sut/next-location recipes locations))))
+    (is (let [recipes [3 7 1 0 1 0 1]
+              locations [4 3]
+              next-locations [6 4]]
+          (= next-locations (sut/next-location recipes locations))))
+    (is (let [recipes [3 7 1 0 1 0 1 2]
+              locations [6 4]
+              next-locations [0 6]]
+          (= next-locations (sut/next-location recipes locations))))
+    ))
+
+;; paren () is the first location
+;; bracket [] is the second location
+ ;; 3  7  1  0 [1] 0 (1)
+(deftest one-elf-next-loc-tests
   (testing "that the next location of the elves is correct"
+    (is (let [ ;; recipes [3 7 1 0 1 0 1]
+              ;; locations [4 3]
+              ;; 3  7  1 [0](1) 0 1 <-- old locations 
+              ;; 3  7  1  0 [1] 0 (1) <--- new locations
+              jump 2
+              recipe-length 7
+              curr-loc 4
+              a-new-loc 6]
+          (= a-new-loc (sut/one-elf-next-loc recipe-length curr-loc jump))))
     (is (let [ ;; recipes [2 1 3]
               ;; locations [0 1]
               jump 3
