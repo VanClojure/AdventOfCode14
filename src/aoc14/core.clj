f7d35f2cd0bab165f98220f1ce6c4644c030eda3
Justin Belanger <justindbelanger@outlook.com>
2019-01-05 12:51:49 -0500

    Add failing test for next-recipes

--- src/aoc14/core.clj
+++ src/aoc14/core.clj
@@ -1,6 +1,10 @@
 (ns aoc14.core)
 
-(defn foo
+(defn next-recipes [recipes]
+  "Given a vector of single-digit integers, each representing a taste-testing score for a hot chocolate recipe, returns the vector with updated scores for the next round of testing."
+  recipes)
+
+#_(defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
--- test/aoc14/core_test.clj
+++ test/aoc14/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [aoc14.core :refer :all]))
 
-(deftest a-test
+#_(deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+(deftest next-recipes-tests
+  (is (= (next-recipes [3 7])
+         [3 7 1 0])))
